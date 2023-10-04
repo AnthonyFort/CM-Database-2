b1db9950ccd24b9b516a5c3494ddf9fdc6c0dc35
Anthony Fort <ajf2195@columbia.edu>
2023-10-04 17:07:01 +0100

    created and began the music app

---
+++ music/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class MusicItem(models.Model):
+    
+    title = models.CharField(max_length=100)
+    composer = models.CharField(max_length=50)
+    related_readings = models.CharField(max_length=100)
+    sheet_music_url = models.URLField()
+    recording = models.URLField()
+    past_performances = models.PositiveIntegerField()
+    comments = models.CharField(max_length=200)
+    # keywords = models.ManyToManyField(
+    #     'keywords.Keyword',
+    #     related_name='music',
+    #     blank=True
+    # )
+    user = models.ForeignKey(
+        'users.User',
+        related_name='music',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+
+
+    
+
