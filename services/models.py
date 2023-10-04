13a0d37ee62afd59cf9688fff0f9b1ff5da9211d
Anthony Fort <ajf2195@columbia.edu>
2023-10-04 16:48:19 +0100

    created services app and started model

---
+++ services/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Service(models.Model):
+    
+    date_of_service:models.DateField()
+    type_of_service: models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.date_of_service} - {self.type_of_service}"
