50510309ccaab71143eef151ddebe93589e8755e
Anthony Fort <ajf2195@columbia.edu>
2023-10-05 12:27:49 +0100

    slight edit to views in keywords folder to allow for keywords list to be retrieved

--- keywords/views.py
+++ keywords/views.py
@@ -7,9 +7,11 @@ from .serializers.populated import PopulatedKeywordSerializer
 class KeywordListView(ListCreateAPIView):
   queryset = Keyword.objects.all()
 
-  def get_serializer(self):
+  def get_serializer(self, *args, **kwargs):
     if self.request.method == 'POST':
-      return KeywordSerializer
-    return PopulatedKeywordSerializer
+      serializer_class = KeywordSerializer
+    else:
+      serializer_class = PopulatedKeywordSerializer
+    return serializer_class(*args, **kwargs)
 
 
