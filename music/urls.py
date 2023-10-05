b6c64597f7f9e96ba1ac72c5422f0b8f0773dbe2
Anthony Fort <ajf2195@columbia.edu>
2023-10-05 11:12:22 +0100

    fixed error in urls.py in music app

--- music/urls.py
+++ music/urls.py
@@ -2,6 +2,6 @@ from .views import MusicItemListView, MusicItemDetailView
 from django.urls import path
 
 urlpatterns = [
-  path('', MusicItemListView()),
-  path('<int:pk>', MusicItemDetailView())
+  path('', MusicItemListView.as_view()),
+  path('<int:pk>', MusicItemDetailView.as_view())
 ]
