b95270f52c49c7f3d15668c5b09e357eaaa3694b
Anthony Fort <ajf2195@columbia.edu>
2023-10-05 11:04:57 +0100

    wrote views for music app

--- music/views.py
+++ music/views.py
@@ -2,6 +2,26 @@ from django.shortcuts import render
 from rest_framework.generics import (
   GenericAPIView, RetrieveUpdateDestroyAPIView, UpdateAPIView
 )
+from lib.views import UserListCreateAPIView
 
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from lib.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+from .models import MusicItem
+
+from .serializers.common import MusicItemSerializer
+
+from rest_framework.response import Response
+
+class MusicItemView(GenericAPIView):
+  queryset=MusicItem.objects.all()
+  serializer_class=MusicItemSerializer
+
+class MusicItemListView(MusicItemView, UserListCreateAPIView):
+  permission_classes = [IsAuthenticated]  
+
+class MusicItemDetailView(MusicItemView, RetrieveUpdateDestroyAPIView):
+  permission_classes = [IsOwnerOrReadOnly]
+
+  def patch(self, request, *args, **kwargs):
+    music_item = self.get_object()
