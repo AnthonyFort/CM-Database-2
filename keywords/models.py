58341c15fb606a5c15f6ac81071ce851ad4b37f8
Anthony Fort <ajf2195@columbia.edu>
2023-10-05 12:59:41 +0100

    created many to many relationship between keyword and music item but at the moment it is not storing specific musical items in the keyword list

--- keywords/models.py
+++ keywords/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 class Keyword(models.Model):
   keyword = models.CharField(max_length=300)
+  related_music_items = models.ManyToManyField(
+        'music.MusicItem',
+        related_name='related_keywords',
+    )
 
 def __str__(self):
   return self.keyword  
--- music/serializers/common.py
+++ music/serializers/common.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 from ..models import MusicItem
+from keywords.serializers.common import KeywordSerializer
+from keywords.models import Keyword
 
 class MusicItemSerializer(serializers.ModelSerializer):
-  # keywords = serializers.CharField()
+  keywords = KeywordSerializer(many=True)
   class Meta:
     model = MusicItem
     fields = '__all__'
 
+  def create(self, validated_data):  
+    keywords_data = validated_data.pop('keywords')
+    music_item = MusicItem.objects.create(**validated_data)
+    
+    for keyword_data in keywords_data:
+      keyword, created = Keyword.objects.get_or_create(**keyword_data)
+      music_item.keywords.add(keyword)
+
+    return music_item    
