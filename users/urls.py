e3839971668b6aff94a251c8aa7e96f8cf9b3ec1
Anthony Fort <ajf2195@columbia.edu>
2023-10-06 16:44:50 +0100

    added logout path in user urls

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView, UserListView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', UserListView.as_view()),
     path('login/', TokenObtainPairView.as_view()),
     path('register/', RegisterView.as_view()),
-    path('refresh/', TokenRefreshView.as_view())
+    path('refresh/', TokenRefreshView.as_view()),
+    path('logout/', LogoutView.as_view())
 ]
