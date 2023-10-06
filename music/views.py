b5bbcdd0083d9d53a1672ab527f6e85f45414e68
Anthony Fort <ajf2195@columbia.edu>
2023-10-06 15:51:55 +0100

    added update reading feature

--- music/views.py
+++ music/views.py
@@ -16,6 +16,7 @@ from rest_framework.response import Response
 import requests
 
 from keywords.models import Keyword
+from readings.models import RelatedReading
 
 
 class MusicItemView(GenericAPIView):
@@ -45,28 +46,41 @@ class MusicItemListView(MusicItemView, UserListCreateAPIView):
           response = super(MusicItemListView, self).create(request, *args, **kwargs)       
           return response
 
-     
-    
-           
-
 class MusicItemDetailView(MusicItemView, RetrieveUpdateDestroyAPIView):
   permission_classes = [IsOwnerOrReadOnly]
-
-  # def patch(self, request, *args, **kwargs):
-  #   music_item = self.get_object()
 
   def patch(self, request, *args, **kwargs):
     if request.method == 'PATCH':
         music_item = self.get_object()
 
-        keywords = request.data.get('keywords')
+        keywords = request.data.get('keywords', [])
         
         for keyword_data in keywords:
             keyword = keyword_data.get('keyword')
             keyword, created = Keyword.objects.get_or_create(keyword=keyword)  
             music_item.keywords.add(keyword)
 
+        related_readings_data = request.data.get('related_readings', [])
+        for reading_data in related_readings_data:
+            book = reading_data.get('book')
+            chapter = reading_data.get('chapter')
+            start_verse = reading_data.get('start_verse')
+            end_verse = reading_data.get('end_verse')
+
+            bible_api_url = f"http://bible-api.com/{book} {chapter}:{start_verse}-{end_verse}"
+            retrieved_text = requests.get(bible_api_url)
+            verse_text = retrieved_text.json().get('text')
+            reading_data['text'] = verse_text
+
+            related_reading, created = RelatedReading.objects.get_or_create(
+                book = book,
+                chapter = chapter,
+                start_verse = start_verse,
+                end_verse = end_verse,
+                text = verse_text
+            )
+            music_item.related_readings.add(related_reading)
+
         music_item.save()
-        print(music_item)
         serializer = self.get_serializer(music_item)
         return Response(serializer.data)
