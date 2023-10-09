e6752472085a28a954b916b84148af7572a7c56e
Anthony Fort <ajf2195@columbia.edu>
2023-10-09 16:33:40 +0100

    set up front end routes in app.js and imported auth.js and common.js files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
-import { useEffect } from 'react'
 import axiosAuth from './lib/axios'
-import Login from './components/Login'
 import { getToken } from './lib/auth'
+
+import AddService from './components/AddService'
+import ChurchPage from './components/ChurchPage'
+import ChurchSearch from './components/ChurchSearch'
+import FormPage from './components/FormPage'
+import Home from './components/Home'
+import Login from './components/Login'
+import MusicSearch from './components/MusicSearch'
+import NavBar from './components/Nav'
+import NotFound from './components/NotFound'
+import Register from './components/Register'
+import SavedMusicItems from './components/SavedMusicItems'
 
 export default function App() {
 
---
+++ client/src/components/Register.js
@@ -0,0 +1,27 @@
+import FormPage from './FormPage'
+import axios from 'axios'
+
+export default function Register() {
+  const fields = [
+    {
+      type: 'text',
+      name: 'Username',
+    },
+    {
+      type: 'password',
+      name: 'Password',
+    },
+    {
+      type: 'password',
+      name: 'Password Confirmation',
+    }
+  ]
+
+  function register(formData){
+    return axios.post('/api/register', formData)
+  }
+
+  return (
+    <FormPage title="Register" request={register} fields={fields} redirect="/login" />
+  )
+}
