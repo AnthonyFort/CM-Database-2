12f202af97d1402490e9b6518b674816341d608d
Anthony Fort <ajf2195@columbia.edu>
2023-10-09 14:18:46 +0100

    relocated auth.js and axios.js to correct lib folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import { useEffect } from 'react'
-import axios from 'axios'
+import axiosAuth from './lib/axios'
+import Login from './components/Login'
+import { getToken } from './lib/auth'
 
 export default function App() {
-  useEffect(() => {
-    async function getData(){
-      try {
-        const { data } = await axios.get('/api/music/') // <---- Replace with your endpoint to test the proxy
-        console.log(data)
-      } catch (error) {
-        console.log(error.response.data)
-      }
-    }
-    getData()
-  }, [])
+  // useEffect(() => {
+  //   async function getData(){
+  //     try {
+  //       const { data } = await axiosAuth.get('/api/books/') // <---- Replace with your endpoint to test the proxy
+  //       console.log(data)
+  //     } catch (error) {
+  //       console.log(error.message)
+  //     }
+  //   }
+  //   getData()
+  // }, [])
 
-  return <h1>Hello World</h1>
+  return <Login />
 }
