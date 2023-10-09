9d45065232a8c6c2c918d25663f1cdbca3fe4efa
Anthony Fort <ajf2195@columbia.edu>
2023-10-09 11:47:14 +0100

    changed service model to introduce unique together constraint for date type and user

--- services/models.py
+++ services/models.py
@@ -4,7 +4,7 @@ from datetime import date
 # Create your models here.
 class Service(models.Model):
     
-    date_of_service = models.DateField(default=date.today, unique=True)
+    date_of_service = models.DateField(default=date.today)
     type_of_service = models.CharField(max_length=50, default='sunday service')
     user = models.ForeignKey(
         'users.User', 
@@ -16,6 +16,8 @@ class Service(models.Model):
         related_name='performances',
     )
 
+    class Meta:
+        unique_together = [['date_of_service', 'type_of_service', 'user']]
 
     def __str__(self):
         return f"{self.date_of_service} - {self.type_of_service}"
