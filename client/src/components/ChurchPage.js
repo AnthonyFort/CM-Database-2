c1c8941c34437635025f08652f9fa7f565074a15
Anthony Fort <ajf2195@columbia.edu>
2023-10-10 13:09:51 +0100

    made keywords and reading inputs dynamic to allow for multiple entries

--- client/src/components/ChurchPage.js
+++ client/src/components/ChurchPage.js
@@ -1,15 +1,13 @@
+
+
 import { useEffect, useState } from 'react'
 import { useNavigate, useParams, Link } from 'react-router-dom'
-import axios from 'axios'
 import ErrorModal from './ErrorModal'
 import axiosAuth from '../lib/axios'
 
-import { getToken } from '../utils/auth'
-
 export default function ChurchPage() {
-
   const navigate = useNavigate()
-  const [ChurchPage, setChurchPage] = useState({
+  const [churchPageData, setChurchPageData] = useState({
     userSongs: [],
   })
   const [churchData, setChurchData] = useState(null)
@@ -19,17 +17,25 @@ export default function ChurchPage() {
   const [newService, setNewService] = useState({
     date_of_service: '',
     type_of_service: '',
-    music_items: [{
-      title: '',
-      composer: '',
-      keywords: '',
-      related_readings: {
-        book: '',
-        chapter: '',
-        start_verse: '',
-        end_verse: '',
-      },
-    }],
+    music_items: [
+      {
+        title: '',
+        composer: '',
+        keywords: [
+          {
+            keyword: '',
+          }
+        ],
+        related_readings: [
+          {
+            book: '',
+            chapter: '',
+            start_verse: '',
+            end_verse: '',
+          }
+        ],
+      }
+    ],
   })
 
   useEffect(() => {
@@ -47,7 +53,6 @@ export default function ChurchPage() {
 
   const handleSubmit = async (event) => {
     event.preventDefault()
-
     try {
       await axiosAuth.post('/api/services/', newService)
     } catch (error) {
@@ -64,35 +69,74 @@ export default function ChurchPage() {
   const addMusicItem = () => {
     setNewService({
       ...newService,
-      music_items: [...newService.music_items, {
-        title: '',
-        composer: '',
-        keywords: '',
-        related_readings: {
-          book: '',
-          chapter: '',
-          start_verse: '',
-          end_verse: '',
-        },
-      }],
+      music_items: [
+        ...newService.music_items,
+        {
+          title: '',
+          composer: '',
+          keywords: [],
+          related_readings: [
+            {
+              book: '',
+              chapter: '',
+              start_verse: '',
+              end_verse: '',
+            }
+          ],
+        }
+      ],
     })
   }
 
-  const handleReadingChange = (index, key, value) => {
+  const handleReadingChange = (index, readingIndex, key, value) => {
     const updatedMusicItems = [...newService.music_items]
-    updatedMusicItems[index].related_readings[key] = value
+    if (!updatedMusicItems[index].related_readings[readingIndex]) {
+      updatedMusicItems[index].related_readings[readingIndex] = {
+        book: '',
+        chapter: '',
+        start_verse: '',
+        end_verse: '',
+      }
+    }
+    updatedMusicItems[index].related_readings[readingIndex][key] = value
     setNewService({ ...newService, music_items: updatedMusicItems })
   }
-  
+
+  const addReading = (index) => {
+    const updatedMusicItems = [...newService.music_items]
+    if (!updatedMusicItems[index].related_readings) {
+      updatedMusicItems[index].related_readings = []
+    }
+    updatedMusicItems[index].related_readings.push({
+      book: '',
+      chapter: '',
+      start_verse: '',
+      end_verse: '',
+    })
+    setNewService({ ...newService, music_items: updatedMusicItems })
+  }
+
+  const handleKeywordChange = (index, keywordIndex, value) => {
+    const updatedMusicItems = [...newService.music_items]
+    updatedMusicItems[index].keywords[keywordIndex].keyword = value
+    setNewService({ ...newService, music_items: updatedMusicItems })
+  }
+
+  const addKeyword = (index) => {
+    const updatedMusicItems = [...newService.music_items]
+    if (!updatedMusicItems[index].keywords) {
+      updatedMusicItems[index].keywords = []
+    }
+    updatedMusicItems[index].keywords.push({ keyword: '' })
+    setNewService({ ...newService, music_items: updatedMusicItems })
+  }
 
   if (!churchData) return <div>Loading...</div>
 
   return (
     <>
       {showErrorModal && <ErrorModal closeModal={() => setShowErrorModal(false)} />}
-
       <h1>{churchData.church}</h1>
-
       <section>
         <h2>Past Services</h2>
         {churchData.past_services.map((service) => (
@@ -107,14 +151,6 @@ export default function ChurchPage() {
                     {item.related_readings.map((reading, index) => (
                       <li key={index}>
                         {reading.book} {reading.chapter}:{reading.start_verse}-{reading.end_verse}
-                      </li>
-                    ))}
-                  </ul>
-                  <ul>
-                    <p>Keywords</p>
-                    {item.keywords.map((keyword, index) => (
-                      <li key={index}>
-                        {keyword.keyword}
                       </li>
                     ))}
                   </ul>
@@ -144,79 +180,78 @@ export default function ChurchPage() {
             />
           </label>
           <button type='button' onClick={addMusicItem}>Add Music Item</button>
-          <div>
-            {newService.music_items.map((musicItem, index) => (
-              <div key={index}>
-                <label>
-                  Title:
-                  <input
-                    value={musicItem.title}
-                    onChange={event => handleMusicItemChange(index, 'title', event.target.value)}
-                  />
-                </label>
+          {newService.music_items.map((musicItem, index) => (
+            <div key={index}>
+              <label>
+                Title:
+                <input
+                  value={musicItem.title}
+                  onChange={event => handleMusicItemChange(index, 'title', event.target.value)}
+                />
+              </label>
 
-                <label>
-                  Composer:
-                  <input
-                    value={musicItem.composer}
-                    onChange={event => handleMusicItemChange(index, 'composer', event.target.value)}
-                  />
-                </label>
-                <label>
-                  Keywords:
-                  <input
-                    value={musicItem.keywords}
-                    onChange={event => handleMusicItemChange(index, 'keywords', event.target.value)}
-                  />
-                </label>
-                <div>
-                  <h4>Related Readings</h4>
-
-                  <label>
-                    Book:
-                    <input
-                      value={musicItem.related_readings.book}
-                      onChange={e => handleReadingChange(index, 'book', e.target.value)}
-                    />
-                  </label>
-
-                  <label>
-                    Chapter:
-                    <input
-                      type="number"
-                      value={musicItem.related_readings.chapter}
-                      onChange={e => handleReadingChange(index, 'chapter', e.target.value)}
-                    />
-                  </label>
-
-                  <label>
-                    Start Verse:
-                    <input
-                      type="number"
-                      value={musicItem.related_readings.start_verse}
-                      onChange={e => handleReadingChange(index, 'start_verse', e.target.value)}
-                    />
-                  </label>
-
-                  <label>
-                    End Verse:
-                    <input
-                      type="number"
-                      value={musicItem.related_readings.end_verse}
-                      onChange={e => handleReadingChange(index, 'end_verse', e.target.value)}
-                    />
-                  </label>
-                </div>
-
+              <label>
+                Composer:
+                <input
+                  value={musicItem.composer}
+                  onChange={event => handleMusicItemChange(index, 'composer', event.target.value)}
+                />
+              </label>
+              <div>
+                {musicItem.keywords.map((k, keywordIndex) => (
+                  <div key={keywordIndex}>
+                    <label>
+                      Keyword:
+                      <input
+                        value={k.keyword}
+                        onChange={e => handleKeywordChange(index, keywordIndex, e.target.value)}
+                      />
+                    </label>
+                  </div>
+                ))}
+                <button onClick={() => addKeyword(index)}>Add Keyword</button>
               </div>
-            ))}
-          </div>
-
-          <button type="submit>">Add Service</button>
+              <div>
+                <h4>Related Readings</h4>
+                {musicItem.related_readings.map((reading, readingIndex) => (
+                  <div key={readingIndex}>
+                    <label>
+                      Book:
+                      <input
+                        value={reading.book}
+                        onChange={event => handleReadingChange(index, readingIndex, 'book', event.target.value)}
+                      />
+                    </label>
+                    <label>
+                      Chapter:
+                      <input
+                        value={reading.chapter}
+                        onChange={event => handleReadingChange(index, readingIndex, 'chapter', event.target.value)}
+                      />
+                    </label>
+                    <label>
+                      Start Verse:
+                      <input
+                        value={reading.start_verse}
+                        onChange={event => handleReadingChange(index, readingIndex, 'start_verse', event.target.value)}
+                      />
+                    </label>
+                    <label>
+                      End Verse:
+                      <input
+                        value={reading.end_verse}
+                        onChange={event => handleReadingChange(index, readingIndex, 'end_verse', event.target.value)}
+                      />
+                    </label>
+                  </div>
+                ))}
+                <button onClick={() => addReading(index)}>Add Reading</button>
+              </div>
+            </div>
+          ))}
+          <button type="submit">Add Service</button>
         </form>
       </section>
-
     </>
   )
-
 }
