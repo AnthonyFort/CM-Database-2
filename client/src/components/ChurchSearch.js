b58ca90503876d7f2180af38308d71cb3178ae80
Anthony Fort <ajf2195@columbia.edu>
2023-10-10 09:31:30 +0100

    started church search component

--- client/src/components/ChurchSearch.js
+++ client/src/components/ChurchSearch.js
@@ -1,3 +1,59 @@
-export default function ChurchSearch(){
-  
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+export default function SearchUsers() {
+
+  const [allChurches, setAllChurches] = useState()
+  const [churches, setChurches] = useState()
+
+
+  useEffect(() => {
+    async function getUserData() {
+      try {
+        const { data } = await axios.get('/api/auth')
+        data.sort()
+        setAllChurches(data)
+        setChurches(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getUserData()
+  }, [])
+
+  function handleKeyup(event) {
+    const selectedChurches = [...allChurches]
+    const newSearchedChurches = selectedChurches.filter(church => church.church.toLowerCase().includes(event.target.value.toLowerCase()))
+    setChurches(newSearchedChurches)
+  }
+
+
+
+  const [searchInput, setSearchInput] = useState('')
+  const [searchBy, setSearchBy] = useState('church') 
+  const handleSearchInputChange = (event) => {
+    setSearchInput(event.target.value)
+  }
+  const handleSearchByChange = (event) => {
+    setSearchBy(event.target.value)
+  }
+
+  return (
+    <div>
+      <div className='search-header'>
+        <h1>Search Churches</h1>
+        <input onKeyUp={handleKeyup} placeholder='Search name' />
+      </div>
+      <section className='user-section'>
+        {churches && churches.map(church => {
+          return (
+            <div key={church.id} value={church._id}>
+              {/* <Link to={`/users/${user._id}`}>{user.username}</Link> */}
+            </div>
+          )
+        })}
+      </section>
+    </div>
+  )
 }
--- keywords/models.py
+++ keywords/models.py
@@ -2,10 +2,6 @@ from django.db import models
 
 class Keyword(models.Model):
   keyword = models.CharField(max_length=300)
-  # related_music_items = models.ManyToManyField(
-  #       'music.MusicItem',
-  #       related_name='related_keywords',
-  #   )
 
 def __str__(self):
   return self.keyword  
