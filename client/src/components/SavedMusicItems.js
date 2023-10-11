bc45786d89c603545000668c33555b99de3ad02d
Anthony Fort <ajf2195@columbia.edu>
2023-10-11 11:41:09 +0100

    imported music serializer into saved serializer so that more music information is fetchable from the saved endpoint

--- client/src/components/SavedMusicItems.js
+++ client/src/components/SavedMusicItems.js
@@ -1,3 +1,8 @@
+import axiosAuth from '../lib/axios'
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams, Link } from 'react-router-dom'
+import ErrorModal from './ErrorModal'
+
 export default function SavedMusicItems(){
   
 }
--- saved/serializers/common.py
+++ saved/serializers/common.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from ..models import SavedMusic
+from services.serializers.common import MusicItemSerializer
+
+
 
 class SavedMusicItemSerializer(serializers.ModelSerializer):
+
+  music_item = MusicItemSerializer()
+
   class Meta:
     model = SavedMusic
     fields ='__all__'
