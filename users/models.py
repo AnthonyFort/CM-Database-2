42cc7ebe7606a76d204aa1e15562e47d5f852867
Anthony Fort <ajf2195@columbia.edu>
2023-10-31 10:57:05 +0000

    Removes redundant create function from User Serializer and adds notes

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+  # Extends User model so that users must be affiliated with a church
   church = models.TextField(max_length=100, unique=True)
   
 
--- users/serializers/common.py
+++ users/serializers/common.py
@@ -4,10 +4,14 @@ from django.contrib.auth import get_user_model
 from services.models import Service
 from services.serializers.common import ServiceSerializer
 
+# Each user is uniquely connected to a church
+# Therefore each "user" is also a particular church - "user" and "church" are interchangeable
+
 User = get_user_model()
 
 class RegistrationSerializer(serializers.ModelSerializer):
 
+    # These fields will not be serialized when user information is retrieved later
     password = serializers.CharField(write_only=True)
     password_confirmation = serializers.CharField(write_only=True)
 
@@ -15,6 +19,8 @@ class RegistrationSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'password', 'password_confirmation', 'church')
 
+    # This functions checks to see that the password and password_confirmation match
+    # It pops the password_confirmation because this field no longer needs to be stored
     def validate(self, data):
         password = data.get('password')
         password_confirmation = data.pop('password_confirmation')
@@ -24,24 +30,26 @@ class RegistrationSerializer(serializers.ModelSerializer):
         
         return data
     
+    # Uses django's built-in create_user method and DRF's validated_data
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
     
 class UserSerializer(serializers.ModelSerializer):
+    # Establishes a related field that is constructed through the ServiceSerializer
     past_services = ServiceSerializer(many=True, read_only=True)
     class Meta:
         model = User
         fields = ('id', 'username', 'church', 'past_services')
 
-    def create(self, validated_data):  
-      past_services_data = validated_data.pop('past_services')
-      user = User.objects.create(**validated_data)
+    # def create(self, validated_data):  
+    #   past_services_data = validated_data.pop('past_services')
+    #   user = User.objects.create(**validated_data)
 
-      for past_service_data in past_services_data:
-        past_service, created = Service.objects.get_or_create(**past_service_data)
-        user.past_services.add(past_service)
+    #   for past_service_data in past_services_data:
+    #     past_service, created = Service.objects.get_or_create(**past_service_data)
+    #     user.past_services.add(past_service)
 
-      return user    
+    #   return user    
 
 
